f8ecc190eac013f0e5535d44cb15fba6557552f8
marcioota <marcio.ota@esystem.com.br>
2025-06-03 22:51:51 -0300

    atualizaco 5- ret_03

---
+++ android/app/src/main/kotlin/com/example/ret_03/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ret_03
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,191 +1,125 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:shared_preferences/shared_preferences.dart'; // salvar última URL
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:permission_handler/permission_handler.dart';
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-
-Future<void> _initLocalNotifications() async {
-  const androidSettings = AndroidInitializationSettings(
-      '@mipmap/orange_blue'); // use temporariamente este ícone
-
-  const initSettings = InitializationSettings(android: androidSettings);
-  await flutterLocalNotificationsPlugin.initialize(initSettings);
-
-  // 👇 Cria o canal manualmente
-  const AndroidNotificationChannel channel = AndroidNotificationChannel(
-    'default_channel', // ✅ mesmo nome
-    'Notificações',
-    description: 'Canal padrão de notificações',
-    importance: Importance.max,
-  );
-
-  await flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<
-          AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
-}
-
-void _showForegroundNotification(RemoteMessage message) {
-  final title =
-      message.notification?.title ?? message.data['title'] ?? 'Notificação';
-  final body =
-      message.notification?.body ?? message.data['body'] ?? 'Entre em contato';
-  final processo_id = message.data['processo_id'];
-  final tipo = message.data['tipo'];
-  print('processo_id ${processo_id}');
-  print('tipo ${tipo}');
-  const androidDetails = AndroidNotificationDetails(
-    'default_channel',
-    'Notificações',
-    channelDescription: 'Notificações gerais',
-    importance: Importance.max,
-    priority: Priority.high,
-    playSound: true,
-    icon: '@mipmap/orange_blue', // evite @mipmap/orange se não for white-only
-  );
-
-  const notificationDetails = NotificationDetails(android: androidDetails);
-
-  flutterLocalNotificationsPlugin.show(
-    DateTime.now().millisecondsSinceEpoch ~/ 1000,
-    title,
-    body,
-    notificationDetails,
-  );
-}
-
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  await Firebase.initializeApp();
-  print("📨 (BG) Mensagem em background: ${message.data}");
-  _showForegroundNotification(message);
-}
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await _initLocalNotifications();
-
-  FirebaseMessaging.onBackgroundMessage(
-      _firebaseMessagingBackgroundHandler); // ✅ ADICIONE AQUI
-
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: WebViewScreen(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class WebViewScreen extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
-  _WebViewScreenState createState() => _WebViewScreenState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _WebViewScreenState extends State<WebViewScreen> {
-  InAppWebViewController? _webViewController;
-  String? _fcmToken;
-  bool isLoading = true;
-  String initialUrl = 'https://ctrends.esystem.com.br?';
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    requestNotificationPermission();
-
-    if (Platform.isAndroid) {
-      InAppWebViewController.setWebContentsDebuggingEnabled(true);
-    }
-
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('📨 Mensagem recebida em foreground: ${message.data}');
-      _showForegroundNotification(message);
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
-
-    _loadLastUrl();
-
-    // Firebase + notificações continuam iguais...
-  }
-
-  void requestNotificationPermission() async {
-    final status = await Permission.notification.status;
-    if (!status.isGranted) {
-      await Permission.notification.request();
-    }
-  }
-
-  Future<void> _loadLastUrl() async {
-    final prefs = await SharedPreferences.getInstance();
-    final savedUrl = prefs.getString('last_url');
-    if (savedUrl != null) {
-      setState(() {
-        initialUrl = savedUrl;
-      });
-    }
   }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-      body: Stack(
-        children: [
-          InAppWebView(
-            initialUrlRequest: URLRequest(url: WebUri(initialUrl)),
-            initialOptions: InAppWebViewGroupOptions(
-              crossPlatform: InAppWebViewOptions(
-                javaScriptEnabled: true,
-                useShouldOverrideUrlLoading: true,
-              ),
-            ),
-            onWebViewCreated: (controller) {
-              _webViewController = controller;
-            },
-            onLoadStop: (controller, url) async {
-              final prefs = await SharedPreferences.getInstance();
-              if (url != null) {
-                await prefs.setString('last_url', url.toString());
-              }
-              setState(() {
-                isLoading = false;
-              });
-            },
-            onReceivedError: (controller, request, error) async {
-              // opcional: mostrar página offline
-              print('Erro ao carregar: $error');
-            },
-          ),
-          if (isLoading)
-            Container(
-              color: Colors.white,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset('assets/icon/orange.png', width: 100),
-                    SizedBox(height: 20),
-                    Text(
-                      'Carregando...',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                    ),
-                    CircularProgressIndicator(),
-                  ],
-                ),
-              ),
-            ),
-        ],
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
